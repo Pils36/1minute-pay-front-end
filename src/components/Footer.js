ecc2f8addc195fa2964a86d72df704553093abf4
patrick <patrickodey419@gmail.com>
2022-04-08 12:35:23 +0100

    download section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,25 @@ import React from 'react'
 function Footer() {
   return (
     <section className="footer">
-      
+      <div className="countdown">
+        <div className="row">
+          <div className="col-12 col-md-3">
+            <div className="countdown-heading">
+            Let the Numbers speak for Us.
+            </div>
+          </div>
+          <div className="col-12 col-md-3">
+            <div className="countdown-number">
+            97,084
+            </div>
+            <div className="countdown-text">
+            Happy Clients
+            </div>
+          </div>
+          <div className="col-12 col-md-3"></div>
+          <div className="col-12 col-md-3"></div>
+        </div>
+      </div>
     </section>
   )
 }
