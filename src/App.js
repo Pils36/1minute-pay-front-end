eccf361eb78af7d7379e889b781461a5574bc5ac
patrick <patrickodey419@gmail.com>
2022-04-08 11:48:24 +0100

    download section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.scss';
 import Download from './components/Download';
+import Footer from './components/Footer';
 import Header from './components/Header';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <Download />
+      <Footer />
     </div>
   );
 }
