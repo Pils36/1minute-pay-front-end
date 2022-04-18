b0ef840556a04e233beedd46d16a8ae31493c8d6
patrick <patrickodey419@gmail.com>
2022-04-18 15:23:35 +0100

    coin carousel mobile

--- src/components/CoinCarousel.js
+++ src/components/CoinCarousel.js
@@ -19,16 +19,62 @@ function CoinCarousel() {
     <section className="coin-carousel">
       <div className="container">
         <div className="row">
-          <div className="col-12">
-            <img src={Binance} alt="binance" />
-            <img src={Tether} alt="tether" />
-            <img src={Tron} alt="tron" />
-            <img src={Bitcoin} alt="bitcoin" />
-            <img src={Ethereum} alt="ethereum" />
-            <img src={Polygon} alt="polygon" />
-            <img src={Litecoin} alt="litecoin" />
-            <img src={Infinite} alt="infinity" />
+          <div className="col-12 d-none d-sm-none d-md-block d-lg-block">
+            <div className='d-flex justify-content-center align-items-center'>
+              <div className='mx-1'>
+              <img src={Binance} className="img-small" alt="binance" />
+              </div>
+              <div className='mx-1'>
+                <img src={Tether} className="img-small" alt="tether" />
+              </div>
+              <div className='mx-1'>
+                <img src={Tron} className="img-small" alt="tron" />
+              </div>
+              <div className='mx-1'>
+                <img src={Bitcoin} className="img-big" alt="bitcoin" />
+              </div>
+              <div className='mx-1'>
+                <img src={Ethereum} className="img-small" alt="ethereum" />
+              </div>
+              <div className='mx-1'>
+                <img src={Polygon} className="img-small" alt="polygon" />
+              </div>
+              <div className='mx-1'>
+                <img src={Litecoin} className="img-small" alt="litecoin" />
+              </div>
+              <div className='mx-1'>
+                <img src={Infinite} className="img-small" alt="infinity" />
+              </div>
+            </div>
+          </div>
 
+          <div className="col-12 d-block d-sm-block d-md-none d-lg-none">
+            <div className='d-flex justify-content-center align-items-center'>
+              <div className='mx-1'>
+              <img src={Binance} className="img-small-mobile" alt="binance" />
+              </div>
+              <div className='mx-1'>
+                <img src={Tether} className="img-small-mobile" alt="tether" />
+              </div>
+              <div className='mx-1'>
+                <img src={Tron} className="img-small-mobile" alt="tron" />
+              </div>
+              <div className='mx-1'>
+                <img src={Bitcoin} className="img-big-mobile" alt="bitcoin" />
+              </div>
+              <div className='mx-1'>
+                <img src={Ethereum} className="img-small-mobile" alt="ethereum" />
+              </div>
+              <div className='mx-1'>
+                <img src={Polygon} className="img-small-mobile" alt="polygon" />
+              </div>
+              <div className='mx-1'>
+                <img src={Litecoin} className="img-small-mobile" alt="litecoin" />
+              </div>
+              <div className='mx-1'>
+                <img src={Infinite} className="img-small-mobile" alt="infinity" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Coins.js
+++ src/components/Coins.js
@@ -4,13 +4,16 @@ function Coins() {
   return (
     <section className="coins">
       <div className="container">
-        <div className="row">
+      <div className="row">
+        <div className="col-12 col-sm-12 col-md-10 col-lg-9 m-auto">
+          <div className="container">
             <div className="coin-header">
-              Designed with ease of use and speed in mind.
+              Designed with ease of use and <br/> speed in mind.
             </div>
           </div>
-  
-      </div>
+        </div>
+      </div>  
+      </div>  
     </section>
   )
 }
