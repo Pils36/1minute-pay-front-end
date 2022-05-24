ce42ed8d2f70abf1e38b4b55ca7237df71196e1a
ekeoma1 <ekeigwee@gmail.com>
2022-05-24 14:00:42 +0100

    reduced the size of lottie loader

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,7 +27,7 @@ function Header(props) {
       {props.children}
       {loading && (
         <div className="loader-wrapper">
-          <Lottie options={defaultOptions} />
+          <Lottie options={defaultOptions} width={250} height={250} />
         </div>
       )}
 
